e6050c3e1d94004fd6c01781c7f99a40eb55863a
ventiv 111 <1841013744@qq.com>
2024-04-23 08:17:35 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,95 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await printPackageInformation('http');
+  print('');
+  await printPackageInformation('path');
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+Future<void> printPackageInformation(String packageName) async {
+  final PackageInfo packageInfo;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  try {
+    packageInfo = await getPackage(packageName);
+  } on PackageRetrievalException catch (e) {
+    print(e);
+    return;
+  }
+
+  print('Information about the $packageName package:');
+  print('Latest version: ${packageInfo.latestVersion}');
+  print('Description: ${packageInfo.description}');
+  print('Publisher: ${packageInfo.publisher}');
+
+  final repository = packageInfo.repository;
+  if (repository != null) {
+    print('Repository: $repository');
+  }
+}
+
+Future<PackageInfo> getPackage(String packageName) async {
+  // 更改获取包信息的网址，这里假设使用了一个不同的域名
+  final packageUrl = Uri.https('example.com', '/packages/$packageName.json');
+  final packageResponse = await http.get(packageUrl);
+
+  // 如果请求不成功，抛出异常
+  if (packageResponse.statusCode != 200) {
+    throw PackageRetrievalException(
+      packageName: packageName,
+      statusCode: packageResponse.statusCode,
+    );
+  }
+
+  final packageJson = json.decode(packageResponse.body) as Map<String, dynamic>;
+
+  return PackageInfo.fromJson(packageJson);
+}
+
+class PackageInfo {
+  final String name;
+  final String latestVersion;
+  final String description;
+  final String publisher;
+  final Uri? repository;
+
+  PackageInfo({
+    required this.name,
+    required this.latestVersion,
+    required this.description,
+    required this.publisher,
+    this.repository,
+  });
+
+  factory PackageInfo.fromJson(Map<String, dynamic> json) {
+    final repository = json['repository'] as String?;
+
+    return PackageInfo(
+      name: json['name'] as String,
+      latestVersion: json['latestVersion'] as String,
+      description: json['description'] as String,
+      publisher: json['publisher'] as String,
+      repository: repository != null ? Uri.tryParse(repository) : null,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class PackageRetrievalException implements Exception {
+  final String packageName;
+  final int? statusCode;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  PackageRetrievalException({required this.packageName, this.statusCode});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  String toString() {
+    final buf = StringBuffer();
+    buf.write('Failed to retrieve package:$packageName information');
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (statusCode != null) {
+      buf.write(' with a status code of $statusCode');
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    buf.write('!');
+    return buf.toString();
   }
 }
